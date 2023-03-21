af491fdc6a426ca2dfa17a4fc96bb02298064390
victornwaforu <nwaforv998@gmail.com>
2023-03-21 18:22:22 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var cors = require('cors');
+require('dotenv').config()
+var multer = require("multer");
+
+
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+var upload = multer({ dest: "./public/data/uploads/" })
+
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post("/api/fileanalyse", upload.single("upfile"), function (req, res) {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
+});
